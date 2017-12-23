e1cc6a78bfc1c7d129e81c17f5ad046aae35fb3b
Dmitri Chebotarev <dmitri.chebotarev@gmail.com>
2017-12-23 20:12:09 +0100

    Added test to dice to check range

--- dice/dice_test.go
+++ dice/dice_test.go
@@ -12,11 +12,11 @@ func init(){
 func TestNewDice(t *testing.T) {
 
 	if d.count != 2 {
-		t.Fatalf("The count of dice should be 2")
+		t.Fatalf("The count of dice should be 2\n")
 	}
 
 	if len(d.Thrown) != 0 {
-		t.Fatalf("The Thrown property should be empty")
+		t.Fatalf("The Thrown property should be empty\n")
 	}
 }
 
@@ -25,6 +25,12 @@ func TestThrow(t *testing.T) {
 	d.Throw()
 
 	if len(d.Thrown) != 2 {
-		t.Fatalf("The Thrown property should be equal to length 2")
+		t.Fatalf("The Thrown property should be equal to length 2, got %d => %v\n", len(d.Thrown), d.Thrown)
+	}
+
+	for _, thrownNumber := range d.Thrown {
+		if thrownNumber < 1 || thrownNumber > 6 {
+			t.Fatalf("The thrown number is out of range %d\n", thrownNumber)
+		}
 	}
 }
