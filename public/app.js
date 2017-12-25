25d07f961f66a511005cb9b0687790eba71953ca
Dmitri Chebotarev <dmitri.chebotarev@gmail.com>
2017-12-25 13:44:11 +0100

    Added a start and lobby screen

--- public/app.js
+++ public/app.js
@@ -4,40 +4,46 @@ new Vue({
     el: '#app',
 
     data: {
-        ws: null // websocket
+        ws: null, // websocket
+        screen: "start"
     },
     
     methods:{
-      register: function () {
-        var name = prompt("What's your name?", "Player");
+        // Linked to the
 
-        if (name.length !== 0) {
-            this.ws.send(JSON.stringify({
-                action: "register",
-                data: name
-            }));
-        }
-        else{
-            this.register(); // Retry
-        }
-      },
-      throwDice: function(){
+        start: function () {
+            var self = this;
+            this.ws = new WebSocket('ws://' + window.location.host + '/ws');
+
+            this.ws.onmessage = function (event) {
+                var json = JSON.parse(event.data);
+
+                switch(json.action) {
+                    case 'register': self.register(); break;
+                    case 'register_ok': self.goToLobby(); break;
+                }
+            }
+        },
+        goToLobby: function () {
+            this.screen = "lobby";
+        },
+        register: function () {
+            var name = prompt("What's your name?", "Player");
+
+            if (name.length !== 0) {
+                this.ws.send(JSON.stringify({
+                    action: "register",
+                    data: name
+                }));
+            }
+            else{
+                this.register(); // Retry
+            }
+        },
+        throwDice: function(){
           this.ws.send(JSON.stringify({
               action: "throw_dice"
           }));
-      }
-    },
-    created: function () {
-        var self = this;
-        this.ws = new WebSocket('ws://' + window.location.host + '/ws');
-
-        this.ws.onmessage = function (event) {
-            var json = JSON.parse(event.data);
-            console.log(json);
-
-            switch(json.action) {
-                case 'register': self.register(); break;
-            }
         }
     }
 });
