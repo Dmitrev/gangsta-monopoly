f9bcf45c564c39da2166992b97f6dcdeff85d2fd
Dmitr <dmitri.chebotarev@gmail.com>
2018-02-25 13:08:16 +0100

    Fixed npm run dev for Windows

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,7 @@
 const mix = require('laravel-mix');
 
+// Fix path issue on Windows
+mix.setPublicPath('./');
 
 mix.sass('assets/scss/style.scss', 'public/dist/css')
     .js('assets/js/index.js', 'public/dist/js');
